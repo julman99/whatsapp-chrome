4c34708767368cb4c3b5592ffd46700911a850c7
Julio Viera <julio.viera@gmail.com>
2015-01-24 00:58:13 -0500

    Improved disable-notification script

--- disable-notification.js
+++ disable-notification.js
@@ -1,12 +1,13 @@
 (function() {
     console.log('Loaded Notification disable');
 
-    window.Notification = function () {
-        this.permission = "granted";
-        console.log('new notification created');
-
-        this.requestPermission = function (e) {
-            console.log('req per');
-        }
+    window.Notification = function (a, b) {
+        console.log('new notification created',a,b);
     }
+
+    window.Notification.permission="granted";
+    window.Notification.requestPermission = function (e) {
+        console.log('req per');
+    }
+
 }());
