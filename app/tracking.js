7dc186988a43cc32abf1110b18e550b525de10c3
Julio Viera <julio.viera@gmail.com>
2015-01-31 12:26:33 -0500

    Adds a keep alive tracking page view

--- app/tracking.js
+++ app/tracking.js
@@ -2,4 +2,8 @@
     var service = analytics.getService('whatsapp');
     var tracker = service.getTracker('UA-58879588-3');
     tracker.sendAppView('MainView');
+
+    setInterval(function(){
+        tracker.sendAppView('MainView_Alive');
+    }, 60 * 60 * 1000)
 }());
